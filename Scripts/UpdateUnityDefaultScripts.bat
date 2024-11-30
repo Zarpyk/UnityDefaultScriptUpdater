540003527259230acd8ae2d9851c58e781c54c49
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2024-11-30 13:59:51 +0100

    Add HKLM editors

--- Scripts/UpdateUnityDefaultScripts.bat
+++ Scripts/UpdateUnityDefaultScripts.bat
@@ -10,4 +10,14 @@ for /F "delims=" %%a in ('reg query "%unityReg%"') do (
 		xcopy /s /y /q "%modScriptPath%" "%%b %%c\Editor\Data\Resources\ScriptTemplates"
 	)
 )
+
+
+set "unityReg=HKLM\SOFTWARE\Unity Technologies\Installer"
+
+for /F "delims=" %%a in ('reg query "%unityReg%"') do (
+	for /F "tokens=4*" %%b in ('reg query "%%a" /v "%unityLocationVariable%"') do (
+		echo "Copy %modScriptPath% files to %%b %%c\Editor\Data\Resources\ScriptTemplates"
+		xcopy /s /y /q "%modScriptPath%" "%%b %%c\Editor\Data\Resources\ScriptTemplates"
+	)
+)
 pause
